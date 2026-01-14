22313114dcb0b729c74fd0629dded7d8227e0d3b
noahe8 <49010326+noahe8@users.noreply.github.com>
2026-01-14 20:46:24 +0000

    minor changes

--- src/app/components/ui/progress.tsx
+++ src/app/components/ui/progress.tsx
@@ -21,7 +21,7 @@ function Progress({
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
+        className="bg-orange-300 h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 100)}%)` }}
       />
     </ProgressPrimitive.Root>
