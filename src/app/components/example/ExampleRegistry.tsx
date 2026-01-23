31f39dd08c0f4426f90e91f0c3662cfd42de197b
Chris6492 <40203076+Chris6492@users.noreply.github.com>
2026-01-23 22:37:35 +0000

    Added an example folder and two files

--- src/app/components/example/ExampleRegistry.tsx
+++ src/app/components/example/ExampleRegistry.tsx
@@ -3,6 +3,7 @@ import { CarPromptExample } from "./CarPromptExample";
 
 export const exampleRegistry = {
   carPrompt: CarPromptExample,
+
 } as const;
 
 export type ExampleType = keyof typeof exampleRegistry;
