8dfa75e37194ff12460d57f961eea1ed94046889
Chris6492 <40203076+Chris6492@users.noreply.github.com>
2026-01-28 03:25:31 +0000

    Updated

--- src/app/Backend/main.py
+++ src/app/Backend/main.py
@@ -1,5 +1,8 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4.1-nano")
 
